9d12b0a6c533ca7f5be6d22d62d4d5c4ee1f8f2a
Blue Dogerino <bluedogerino@gmail.com>
2018-07-06 00:45:28 +0200

    rebrand common tree to j5x

--- init/init_j5x.cpp
+++ init/init_j5x.cpp
@@ -37,39 +37,24 @@ void init_target_properties(void)
 	/* get the bootloader string */
 	std::string bootloader = android::base::GetProperty("ro.bootloader", "");
 
-	if (bootloader.find("J500FN") == 0) {
-		device = (char *)"j5nltexx";
-		model = (char *)"SM-J500FN";
+	if (bootloader.find("J510FN") == 0) {
+		device = (char *)"j5xnlte";
+		model = (char *)"SM-J510FN";
 		set_lte_properties();
 	}
-	else if (bootloader.find("J500F") == 0) {
-		device = (char *)"j5ltexx";
-		model = (char *)"SM-J500F";
+	else if (bootloader.find("J510F") == 0) {
+		device = (char *)"j5xlte";
+		model = (char *)"SM-J510F";
 		set_lte_properties();
 	}
-	else if (bootloader.find("J500H") == 0) {
-		device = (char *)"j53gxx";
-		model = (char *)"SM-J500H";
+	else if (bootloader.find("J510H") == 0) {
+		device = (char *)"j5x3g";
+		model = (char *)"SM-J510H";
 		set_gsm_properties();
 	}
-	else if (bootloader.find("J500M") == 0) {
-		device = (char *)"j5lteub";
-		model = (char *)"SM-J500M";
-		set_lte_properties();
-	}
-	else if (bootloader.find("J500Y") == 0) {
-		device = (char *)"j5ylte";
-		model = (char *)"SM-J500Y";
-		set_lte_properties();
-	}
-	else if (bootloader.find("J500G") == 0) {
-		device = (char *)"j5ltedx";
-		model = (char *)"SM-J500G";
-		set_lte_properties();
-	}
-	else if (bootloader.find("J5008") == 0) {
-		device = (char *)"j5ltechn";
-		model = (char *)"SM-J5008";
+	else if (bootloader.find("J510MN") == 0) {
+		device = (char *)"j5xnlte";
+		model = (char *)"SM-J510MN";
 		set_lte_properties();
 	}
 	else {
